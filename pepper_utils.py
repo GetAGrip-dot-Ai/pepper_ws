2b4b2e75af60f737d0c21ab80610b6891c05bb4c
jiyooonp <112532636+jiyooonp@users.noreply.github.com>
2023-04-07 16:22:36 -0400

    fixed port changing issue by using import pyrealsense2

--- pepper_utils.py
+++ pepper_utils.py
@@ -3,6 +3,7 @@ import os
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+import pyrealsense2 as rs
 # from realsense_camera import *
 
 def get_img_size(img_path):
@@ -38,7 +39,7 @@ def put_title(detected_frame):
     
 
 def get_image_from_webcam():
-    camera = cv2.VideoCapture(6)
+    camera = cv2.VideoCapture(0)
     # camera = cv2.VideoCapture(0)
     # 4: dotted camera
     # 6: rgb camera
@@ -57,6 +58,82 @@ def get_image_from_webcam():
     camera.release()
     cv2.destroyAllWindows()
     return image
+
+def get_image_from_realsense():
+    # Configure depth and color streams
+    pipeline = rs.pipeline()
+    config = rs.config()
+
+    # Get device product line for setting a supporting resolution
+    pipeline_wrapper = rs.pipeline_wrapper(pipeline)
+    pipeline_profile = config.resolve(pipeline_wrapper)
+    device = pipeline_profile.get_device()
+    device_product_line = str(device.get_info(rs.camera_info.product_line))
+
+    found_rgb = False
+    for s in device.sensors:
+        if s.get_info(rs.camera_info.name) == 'RGB Camera':
+            found_rgb = True
+            break
+    if not found_rgb:
+        print("The demo requires Depth camera with Color sensor")
+        exit(0)
+
+    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+
+    if device_product_line == 'L500':
+        config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
+    else:
+        config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
+    # Start streaming
+    pipeline.start(config)
+
+    try:
+        while True:
+
+            # Wait for a coherent pair of frames: depth and color
+            frames = pipeline.wait_for_frames()
+            depth_frame = frames.get_depth_frame()
+            color_frame = frames.get_color_frame()
+            if not depth_frame or not color_frame:
+                continue
+
+            # Convert images to numpy arrays
+            depth_image = np.asanyarray(depth_frame.get_data())
+            color_image = np.asanyarray(color_frame.get_data())
+
+            # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
+            depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+
+            depth_colormap_dim = depth_colormap.shape
+            color_colormap_dim = color_image.shape
+
+            # If depth and color resolutions are different, resize color image to match depth image for display
+            if depth_colormap_dim != color_colormap_dim:
+                resized_color_image = cv2.resize(color_image, dsize=(depth_colormap_dim[1], depth_colormap_dim[0]), interpolation=cv2.INTER_AREA)
+                images = np.hstack((resized_color_image, depth_colormap))
+            else:
+                images = np.hstack((color_image, depth_colormap))
+
+            # Show images
+            cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
+            cv2.imshow('RealSense', images)
+            
+            k = cv2.waitKey(0)
+            if k==27:
+                print("hey")
+                cv2.destroyAllWindows()
+                print("images", images.shape)
+                return images[:, :640, :]
+                # break
+
+    finally:
+
+        # Stop streaming
+        pipeline.stop()
+
+    return resized_color_image
 
 
 def red_to_green(img):
@@ -104,7 +181,8 @@ def red_to_green_2(img):
     return retrack_original
 
 if __name__=="__main__":
-    img = get_image_from_webcam()
+    # img = get_image_from_webcam()
+    img = get_image_from_realsense()
     cv2.imwrite('he.png', img)
      # plt.imshow(img)
     # plt.imsave(img, "hi.png")
