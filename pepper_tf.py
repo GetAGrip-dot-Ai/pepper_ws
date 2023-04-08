7052dd23a4d8814e619393e5ade56ae58e98409e
jiyooonp <112532636+jiyooonp@users.noreply.github.com>
2023-04-08 16:32:45 -0400

    poi not in peduncle

--- pepper_tf.py
+++ pepper_tf.py
@@ -35,26 +35,17 @@ def tfCallback(msg):
         pepper_tf.transform.translation.y = (y-240)/1000
         pepper_tf.transform.translation.x = depth_img[x][y] /1000
 
-        # rot = t.transform.rotation
-        # rpy = euler_from_quaternion([rot.x, rot.y, rot.z, rot.w])
-        # r = rpy[0] + math.pi
-        # p = rpy[1] + math.pi/4 + math.pi/2
-        # # p = rpy[1] + math.pi/2
-        # y = rpy[2] 
         quat = quaternion_from_euler(0, 0, 0)
         quat_msg = Quaternion(quat[0], quat[1], quat[2], quat[3])
         pepper_tf.transform.rotation = quat_msg
         pepper_tf.header.stamp = rospy.Time.now()
-        # camera_tf.header.frame_id = "bracelet_link"
-        # camera_tf.child_frame_id = "camera_frame"
-        # print(camera_tf.transform.translation.x)
-        # br.sendTransform(camera_tf)
         br.sendTransform((pepper_tf.transform.translation.x, pepper_tf.transform.translation.y, pepper_tf.transform.translation.z),(pepper_tf.transform.rotation.x, pepper_tf.transform.rotation.y, pepper_tf.transform.rotation.z, pepper_tf.transform.rotation.w), time=pepper_tf.header.stamp , child = "pepper", parent="rs_ee")
     else:
         print("waiting for image to be a thing")
+
 def tf_listener():
     rospy.init_node('pp_p_pepper_tf_node')
-    rospy.Subscriber("/camera/pp/depth", Int64MultiArray, getDepthCallback)
+    rospy.Subscriber("/pp/depth_img", Int64MultiArray, getDepthCallback)
     rospy.Subscriber("/tf", TFMessage, tfCallback)
     rospy.Subscriber("/pp/poi_test", Int64MultiArray, xy_callback)
     rospy.spin()
